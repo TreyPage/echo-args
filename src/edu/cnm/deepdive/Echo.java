23c92be3b0e3c117e441eb4f620466d418cff234
Terrell Page <tpage46@gmail.com>
2019-05-20 15:38:36 -0600

    added comments

--- src/edu/cnm/deepdive/Echo.java
+++ src/edu/cnm/deepdive/Echo.java
@@ -1,11 +1,18 @@
-package edu.cnm.deepdive;
+package edu.cnm.deepdive; //Package name lower case
 
-public class Echo {
+public class Echo { // Class name UpperCamelCase
 
-  public static void main(String[] args) {
+  private static final String ARG_ITEM_FORMAT = "%d: %s%n"; // Constant name UPPER_SNAKE_CASE
 
-    for (int i = 0; i < args.length; i++) {
-      System.out.printf("%d: %s%n", i, args[i].toUpperCase());
+  public static void main(String[] args) { // Method name lowerCamelCase
+
+    for (int i = 0; i < args.length; i++) { // Local variable name lowerCamelCase
+      if (args[i].length() > 3) {
+        System.out.printf(ARG_ITEM_FORMAT, i, args[i].toUpperCase());
+      }
+      else {
+        System.out.printf(ARG_ITEM_FORMAT, i, args[i].toLowerCase());
+      }
     }
   }
 
